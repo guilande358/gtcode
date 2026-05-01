384af3445a7045cffcc595bcfcd756dc7ffaf7ff
guilande358 <guilandeelves5@gmail.com>
2026-05-01 09:11:30 +0200

    Refactor Electron type declarations in globals.d.ts

--- electron/src/globals.d.ts
+++ electron/src/globals.d.ts
@@ -1,35 +1,10 @@
 // electron/src/globals.d.ts
 declare module 'electron' {
-  export * from 'electron';
-}
-
-declare module 'electron-serve' {
-  const content: any;
-  export default content;
-}
-
-declare module 'electron-updater' {
-  export const autoUpdater: any;
-}
-
-declare module 'electron-window-state' {
-  const content: any;
-  export default content;
-}
-
-declare module 'electron-is-dev' {
-  const content: boolean;
-  export default content;
-}
-
-declare module 'electron-unhandled' {
-  const content: any;
-  export default content;
-}
-
-// tipos para o processo principal do Electron
-declare namespace NodeJS {
-  interface Process {
-    type: string;
-  }
+  export const app: any;
+  export const BrowserWindow: any;
+  export const ipcMain: any;
+  export const ipcRenderer: any;
+  export const shell: any;
+  export const dialog: any;
+  export const Menu: any;
 }
