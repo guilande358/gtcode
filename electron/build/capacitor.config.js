069c6875809417272593f168c9564abe5e8f60f7
guilande358 <guilandeelves5@gmail.com>
2026-05-01 16:27:39 +0200

    Add files via upload

---
+++ electron/build/capacitor.config.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const config = {
+    appId: 'app.lovable.5302ba4b86704d78a7793d260b16a576',
+    appName: 'GcodeForce Studio',
+    webDir: 'dist',
+    server: {
+        url: 'https://5302ba4b-8670-4d78-a779-3d260b16a576.lovableproject.com?forceHideBadge=true',
+        cleartext: true,
+        androidScheme: 'https',
+    },
+};
+exports.default = config;
